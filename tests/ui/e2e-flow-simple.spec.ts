1aaccf8a055d392f3ccff8d6446ec00d88a3f0d5
Eimante.Bogdan <eimante.bogdan@gmail.com>
2025-11-22 18:12:02 +0200

    Add homework assignment tests using POM

--- tests/ui/e2e-flow-simple.spec.ts
+++ tests/ui/e2e-flow-simple.spec.ts
@@ -1,20 +1,51 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../pages/login-page'
-import { faker } from '@faker-js/faker/locale/ar'
+import { faker } from '@faker-js/faker'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
-test('signIn button disabled when incorrect data inserted', async ({ page }) => {
-  const authPage = new LoginPage(page)
+let authPage: LoginPage
+
+test.beforeEach(async ({ page: _page }) => {
+  authPage = new LoginPage(_page)
   await authPage.open()
+})
+
+test('signIn button disabled when incorrect data inserted', async ({}) => {
   await authPage.usernameField.fill(faker.lorem.word(2))
   await authPage.passwordField.fill(faker.lorem.word(7))
   await expect(authPage.signInButton).toBeDisabled()
 })
 
-test('login with correct credentials and verify order creation page', async ({ page }) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('login and verify elements on "Order creation" page', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
-  await orderCreationPage.statusButton.click({ force: true })
-  // verify at least few elements on the order creation page
+  await expect(orderCreationPage.userNameField).toBeVisible()
+  await expect(orderCreationPage.phoneNumber).toBeVisible()
+  await expect(orderCreationPage.commentField).toBeVisible()
+  await expect(orderCreationPage.orderButton).toBeVisible()
+  await expect(orderCreationPage.statusButton).toBeVisible()
+  await expect(orderCreationPage.mainPageLink).toBeVisible()
+})
+
+test('login and create order successfully', async ({}) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.createOrder()
+  await expect(orderCreationPage.orderCreatedButton).toBeVisible()
+})
+
+test('login and verify validation errors during order creation', async ({}) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.fillOrderFields(
+    faker.lorem.word(1), // invalid name
+    faker.lorem.word(2), // invalid phone
+    faker.lorem.word(), // comment
+  )
+  await expect(orderCreationPage.userNameError).toBeVisible()
+  await expect(orderCreationPage.phoneError).toBeVisible()
+  await expect(orderCreationPage.orderButton).toBeDisabled()
+})
+
+test('login and logout', async ({}) => {
+  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  await orderCreationPage.logout()
+  await expect(authPage.signInButton).toBeVisible()
 })
